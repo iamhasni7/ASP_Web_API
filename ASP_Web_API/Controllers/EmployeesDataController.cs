1fe59bab23e154da6920c79bdbf72f0fe65ef924
Muhammad Hussnain Fareed <hasni.rock@yahoo.com>
2022-11-12 11:52:40 +0500

    GET Method added

---
+++ ASP_Web_API/Controllers/EmployeesDataController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP_Web_API.Controllers
+{
+    public class EmployeesDataController : ApiController
+    {
+        public string[] myEmployees = { "Ali", "Usman", "Hamza" };
+
+        [HttpGet]
+        public string[] GetEmployees()
+        {
+            return myEmployees;
+        }
+
+        [HttpGet]
+        public string GetEmployeeByIndex(int id)
+        {
+            return myEmployees[id];
+        }
+    }
+}
